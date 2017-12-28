0107ff7598fa0c39fa4a396b388b4e338e4978fb
hj47 <sana_1004@naver.com>
2017-12-28 09:11:20 +0900

    add insured

---
+++ insured/insured.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { InsuredComponent } from './insured.component';
+
+describe('InsuredComponent', () => {
+  let component: InsuredComponent;
+  let fixture: ComponentFixture<InsuredComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ InsuredComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(InsuredComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
